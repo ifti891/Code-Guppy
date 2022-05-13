08bba400adb4ad815379131c2ded56d065e521b1
iftikhar <iftikhar.sy@gmail.com>
2022-05-13 14:46:44 +0530

    arrays

---
+++ Array Methods/insertArrayAtbegning.js
@@ -0,0 +1,13 @@
+// Insert an element at the beginning
+
+let ar = [10, 20, 30];
+console.log("Intital array");
+console.log(ar);
+
+console.log("........................................");
+
+// Insert an element at the beginning of an array
+
+ar.unshift(1);
+console.log("\nArray  after unshift i.e adding an element at the begning of an array.");
+console.log(ar);
